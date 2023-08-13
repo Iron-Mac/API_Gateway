651a92e56428f986206819a46c605563bbbdd90c
Hamid <hamid@debian-BULLSEYE-live-builder-AMD64>
2023-08-14 00:34:39 +0330

    Adding phase 1

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from passlib.context import CryptContext
+from pydantic import BaseModel
 from datetime import datetime, timedelta
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Boolean, JSON, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship, Session
+from model_evaluation import print_word_tag
+from TextRankMedium import textrank
+from Roge import roge
 import jwt
+import time
 import requests
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 app = FastAPI()
 security = HTTPBearer()
 
 # Replace this with your own secret key
+DATABASE_URL = "sqlite:///./test.db"  # SQLite database URL
 SECRET_KEY = "your-secret-key"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
@@ -18,17 +29,82 @@ class User:
     def __init__(self, username, password):
         self.username = username
         self.password = password
-        self.get_token_bucket = 20  # Number of GET requests allowed per day
-        self.post_token_bucket = 10  # Number of POST requests allowed per day
 
-# Sample database of users
-users_db = {
-    "john": User("john", "password123"),
-    "jane": User("jane", "password456"),
-}
+class CreateModule(BaseModel):
+    title: str
+    description: str
+    url: str
+    limit: int  # Add a field for rate limit
+
+class ModuleRequest(BaseModel):
+    module_id: int
+    input_data: str
+
+class Input1(BaseModel):
+    input_data: str
+
+class Input2(BaseModel):
+    phrase1: str
+    phrase2: str
+
+class SetRateLimit(BaseModel):
+    url: str
+    limit: int
+
+class AddModuleToUser(BaseModel):
+    username: str
+    module_id: int
+
+# SQLAlchemy setup
+Base = declarative_base()
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Define User model for the database
+class Module(Base):
+    __tablename__ = "modules"
+
+    id = Column(Integer, primary_key=True, index=True)
+    creator_id = Column(Integer, ForeignKey("users.id"))
+    title = Column(String, index=True)
+    description = Column(String)
+    url = Column(String)
+
+    creator = relationship("User", back_populates="modules")
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    password_hash = Column(String)
+    is_admin = Column(Boolean, default=False)
+    url_token_buckets = Column(JSON)
+    # Define the many-to-many relationship with Module
+    modules = relationship("Module", secondary="user_module", back_populates="users")
+
+# Association table for User and Module many-to-many relationship
+class UserModule(Base):
+    __tablename__ = "user_module"
+
+    user_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
+    module_id = Column(Integer, ForeignKey("modules.id"), primary_key=True)
+    limit = Column(Integer, default=0)
+    tokens = Column(Float, default=0.0)
+    last_refill = Column(Float, default=time.time())
+
+
+Base.metadata.create_all(bind=engine)
 
 # Password context for password hashing
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 # Generate access token
 def create_access_token(username: str):
@@ -59,22 +135,30 @@ def verify_password(plain_password: str, hashed_password: str):
 
 # Register a new user
 @app.post("/register")
-def register(username: str, password: str):
-    if username in users_db:
+def register(username: str, password: str, is_admin: bool = False, session: Session = Depends(get_db)):
+    if session.query(User).filter_by(username=username).first():
         raise HTTPException(status_code=400, detail="Username already registered")
+
     hashed_password = get_password_hash(password)
-    user = User(username, hashed_password)
-    users_db[username] = user
+    user = User(username=username, password_hash=hashed_password, is_admin=is_admin, url_token_buckets={})
+    session.add(user)
+
+    # Create a list to store user modules
+    user.modules = []
+    
+    session.commit()
     return {"message": "User registered successfully"}
 
 # Login and generate access token
 @app.post("/login")
-def login(username: str, password: str):
-    if username not in users_db:
+def login(username: str, password: str, session: Session = Depends(get_db)):
+    user = session.query(User).filter_by(username=username).first()
+    if not user:
         raise HTTPException(status_code=401, detail="Invalid username or password")
-    user = users_db[username]
-    if not verify_password(password, user.password):
+
+    if not verify_password(password, user.password_hash):
         raise HTTPException(status_code=401, detail="Invalid username or password")
+
     access_token = create_access_token(username)
     return {"access_token": access_token}
 
@@ -84,80 +168,140 @@ def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(securit
     username = verify_token(token)
     return username
 
-# Protected route example
-@app.get("/protected")
-def protected_route(user: str = Depends(get_current_user)):
-    # Example usage of the user information
-    if user not in users_db:
-        raise HTTPException(status_code=401, detail="Invalid user")
-    return {"message": f"Hello, {user}! This is a protected route."}
-
-# Example of an NLP module that requires the user to be logged in and enforces rate limiting for GET and POST methods separately
-@app.get("/nlp")
-def nlp_module_get(user: str = Depends(get_current_user)):
-    # Check if the user is logged in and authorized to use the NLP module
-    if user not in users_db:
+@app.post("/process-module")
+def process_module(request_data: ModuleRequest, user: str = Depends(get_current_user), session: Session = Depends(get_db)):
+    user_db = session.query(User).filter_by(username=user).first()
+    if not user_db:
         raise HTTPException(status_code=401, detail="Invalid user")
 
-    # Retrieve the user object from the users_db
-    user_obj = users_db[user]
+    module = session.query(Module).filter_by(id=request_data.module_id).first()
+    if not module:
+        raise HTTPException(status_code=404, detail="Module not found")
 
-    # Check rate limit for GET requests
-    if user_obj.get_token_bucket <= 0:
-        raise HTTPException(status_code=429, detail="Rate limit exceeded for GET requests. Please try again later.")
+    # Check if the user has access to the requested module
+    if module not in user_db.modules:
+        raise HTTPException(status_code=403, detail="Access denied to module")
 
-    # Perform NLP operations using the user data
-    # Example: Access user attributes
-    username = user_obj.username
-    password = user_obj.password
+    url_bucket = user_db.url_token_buckets.get(module.url)
+    if not url_bucket:
+        raise HTTPException(status_code=429, detail="Rate limit exceeded for this module")
 
-    # Update the token bucket for GET requests
-    user_obj.get_token_bucket -= 1
+    if url_bucket['tokens'] <= 0:
+        raise HTTPException(status_code=429, detail="Rate limit exceeded for this module. Please try again later.")
 
-    return {"message": f"Hello, {username}! This is the NLP module for GET requests."}
+    # Deduct token and update last refill time
+    now = time.time()
+    time_since_last_refill = now - url_bucket['last_refill']
+    refill_rate = 86400  # 24 hours in seconds
 
-@app.post("/nlp")
-def nlp_module_post(text, user: str = Depends(get_current_user)):
-    # Check if the user is logged in and authorized to use the NLP module
-    print(users_db)
-    if user not in users_db:
-        raise HTTPException(status_code=401, detail="Invalid user")
-    
+    url_bucket['tokens'] = min(url_bucket['tokens'] + time_since_last_refill * (1 / refill_rate), url_bucket['limit'])
 
-    # Retrieve the user object from the users_db
-    user_obj = users_db[user]
+    # Update last refill time
+    url_bucket['last_refill'] = now
 
-    # Check rate limit for POST requests
-    if user_obj.post_token_bucket <= 0:
-        raise HTTPException(status_code=429, detail="Rate limit exceeded for POST requests. Please try again later.")
-
-    # Perform NLP operations using the user data
-    # Example: Access user attributes
-    username = user_obj.username
-    password = user_obj.password
-
-    # Update the token bucket for POST requests
-    user_obj.post_token_bucket -= 1
-
-    SERVICE_URL = f"http://localhost:8001/hash/?text={text}"
+    # Send a request to the module URL with the provided input
     headers = {
         "accept": "application/json",
     }
     data = {
-        "text": "Your actual text here",  # Replace this with the text you want to hash
+        "input_data": request_data.input_data,
     }
-
-    response = requests.post(SERVICE_URL)
-
+    response = requests.post(module.url, headers=headers, json=data)
 
     if response.status_code == 200:
         try:
             result = response.json()  # Parse the JSON response
-            print(result)
-            return {"result": result}
+            return result
         except ValueError:
-            return {"error": "Failed to parse the JSON response from the service."}
+            return {"error": "Failed to parse the JSON response from the module."}
     else:
-        return {"error": "Failed to process the text."}
+        return {"error": "Failed to process the module request."}
+
+@app.post("/set-rate-limit")
+def set_rate_limit(rate_limit_data: SetRateLimit, admin_user: str = Depends(get_current_user), session: Session = Depends(get_db)):
+    admin_db = session.query(User).filter_by(username=admin_user).first()
+    if not admin_db or not admin_db.is_admin:
+        raise HTTPException(status_code=401, detail="Access denied")
+
+    target_user = session.query(User).filter_by(username=rate_limit_data.username).first()
+    if not target_user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    # Get the user's module association with the specified module
+    module = session.query(Module).filter_by(url=rate_limit_data.url).first()
+    if not module:
+        raise HTTPException(status_code=404, detail="Module not found")
+
+    user_module = session.query(UserModule).filter_by(user_id=target_user.id, module_id=module.id).first()
+    if not user_module:
+        raise HTTPException(status_code=404, detail="User does not have access to this module")
+
+    # Update the rate limit for the module
+    user_module.limit = rate_limit_data.limit
+    session.commit()
+
+    return {"message": "Rate limit updated successfully"}
 
 
+# Example of adding a module to a user's list of modules
+@app.post("/add-module-to-user")
+def add_module_to_user(module_data: AddModuleToUser, admin_user: str = Depends(get_current_user), session: Session = Depends(get_db)):
+    admin_db = session.query(User).filter_by(username=admin_user).first()
+    if not admin_db or not admin_db.is_admin:
+        raise HTTPException(status_code=401, detail="Access denied")
+
+    user_db = session.query(User).filter_by(username=module_data.username).first()
+    if not user_db:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    module = session.query(Module).filter_by(id=module_data.module_id).first()
+    if not module:
+        raise HTTPException(status_code=404, detail="Module not found")
+
+    # Add the module to the user's list of modules
+    user_db.modules.append(module)
+
+    session.commit()
+
+    return {"message": "Module added to user successfully"}
+
+@app.post("/create-module")
+def create_module(module_data: CreateModule, user: str = Depends(get_current_user), session: Session = Depends(get_db)):
+    user_db = session.query(User).filter_by(username=user).first()
+    if not user_db:
+        raise HTTPException(status_code=401, detail="Invalid user")
+
+    # Create a new module instance and store it in the database
+    new_module = Module(
+        creator_id=user_db.id,
+        title=module_data.title,
+        description=module_data.description,
+        url=module_data.url
+    )
+    session.add(new_module)
+    
+    # Create a UserModule instance to store rate limit for the module and user
+    user_module = UserModule(user_id=user_db.id, module_id=new_module.id, limit=module_data.limit, tokens=module_data.limit)
+
+    # Add the module to the user's list of modules with rate limit
+    user_db.modules.append(new_module)
+    user_db.url_token_buckets[new_module.url] = {"limit": user_module.limit, "tokens": user_module.tokens, "last_refill": user_module.last_refill}
+
+    session.commit()
+
+    return {"message": "Module created successfully", "module_id": new_module.id}
+
+@app.post("/postager/")
+async def read_root(user_input: Input1):
+    res = print_word_tag(user_input.input_data)
+    return {"res": res}
+
+@app.post("/textrank/")
+async def read_root(user_input: Input1):
+    res = textrank(user_input.input_data)
+    return {"res": res}
+
+@app.post("/roge/")
+async def read_root(user_input: Input2):
+    res = roge(user_input.phrase1, user_input.phrase2)
+    return {"res": res}
